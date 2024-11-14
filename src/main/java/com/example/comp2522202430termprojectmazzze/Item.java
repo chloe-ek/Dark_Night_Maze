7f6a2e0a1a33eb3836f5418453c7f34d20781d92
eunji-cs <ekwon9@my.bcit.ca>
2024-11-13 22:36:45 -0800

    Add Item class to represent collectibles in the maze

---
+++ src/main/java/com/example/comp2522202430termprojectmazzze/Item.java
@@ -0,0 +1,18 @@
+package com.example.comp2522202430termprojectmazzze;
+
+public class Item {
+    private int x, y;
+
+    public Item(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
