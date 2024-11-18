8ada703866e994a537e5a6049e21f88e2bba8aee
eunji-cs <ekwon9@my.bcit.ca>
2024-11-18 01:51:57 -0800

    Refactor single variable names in Position class for clarity

--- src/main/java/com/example/comp2522202430termprojectmazzze/Position.java
+++ src/main/java/com/example/comp2522202430termprojectmazzze/Position.java
@@ -2,19 +2,19 @@ package com.example.comp2522202430termprojectmazzze;
 
 
 public class Position {
-    private final int x;
-    private final int y;
+    private final int coordinateX;
+    private final int coordinateY;
 
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Position(final int coordinateX, final int coordinateY) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
     }
 
     public int getX() {
-        return x;
+        return coordinateX;
     }
 
     public int getY() {
-        return y;
+        return coordinateY;
     }
 }
