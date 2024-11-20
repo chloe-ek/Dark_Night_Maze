ac71206de17e41e8ce1d31133b43c9e27cd8224b
eunji-cs <ekwon9@my.bcit.ca>
2024-11-20 11:04:16 -0800

    Add player image loading mechanism and resource path

--- src/main/java/com/example/comp2522202430termprojectmazzze/Player.java
+++ src/main/java/com/example/comp2522202430termprojectmazzze/Player.java
@@ -2,16 +2,37 @@ package com.example.comp2522202430termprojectmazzze;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 
 
 public class Player implements Character, Updatable {
+    private static final String IMAGE_PATH_LEFT = "/images/redhoodie_left.png";
+    private static final String IMAGE_PATH = "/images/redhoodie.png";
     private Position position;
-
+    private Image playerImage;
+    private Direction currentDirection;
 
     public Player(final Position startPosition) {
         this.position = startPosition;
+        this.currentDirection = Direction.RIGHT;
 
+        loadImage(IMAGE_PATH);
     }
+
+    private void loadImage(final String imagePath) {
+        try {
+            var resourceUrl = getClass().getResource(imagePath);
+            if (resourceUrl == null) {
+                throw new NullPointerException("Resource not found: " + imagePath);
+            }
+            this.playerImage = new Image(resourceUrl.toExternalForm());
+        } catch (Exception e) {
+            System.out.println("Failed to load player image: " + imagePath);
+            e.printStackTrace();
+            this.playerImage = null;
+        }
+    }
+
     @Override
     public Position getPosition() {
         return position;
@@ -22,11 +43,17 @@ public class Player implements Character, Updatable {
         int newX = position.getX() + direction.getDirectionX();
         int newY = position.getY() + direction.getDirectionY();
 
+        if (direction == Direction.LEFT && currentDirection != Direction.LEFT) {
+            currentDirection = Direction.LEFT;
+            loadImage(IMAGE_PATH_LEFT);
+        } else if (direction == Direction.RIGHT && currentDirection != Direction.RIGHT) {
+            currentDirection = Direction.RIGHT;
+            loadImage(IMAGE_PATH);
+        }
         if (isValidMove(newX, newY, maze)) {
             position = new Position(newX, newY);
         }
     }
-
 
     private boolean isValidMove(final int targetX, final int targetY, final boolean[][] maze) {
         return targetX >= 0 && targetX < maze.length
@@ -35,14 +62,16 @@ public class Player implements Character, Updatable {
     }
 
     @Override
-    public void render(final GraphicsContext gc, int tileSize) {
-        gc.setFill(Color.BLUE);
-        gc.fillRect(position.getX() * tileSize, position.getY() * tileSize, tileSize, tileSize);
+    public void render(final GraphicsContext gc, final int tileSize) {
+        if (playerImage != null) {
+            gc.drawImage(playerImage, position.getX() * tileSize, position.getY() * tileSize, tileSize, tileSize);
+        } else {
+            gc.setFill(Color.BLUE);
+            gc.fillRect(position.getX() * tileSize, position.getY() * tileSize, tileSize, tileSize);
+        }
     }
 
     @Override
     public void update(final boolean[][] maze) {
-
     }
-
 }
