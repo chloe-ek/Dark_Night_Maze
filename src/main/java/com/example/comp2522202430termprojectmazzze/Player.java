dfdc1deb08ba98547dc878f4643dcbcb3efcc872
eunji-cs <ekwon9@my.bcit.ca>
2024-11-20 10:35:20 -0800

    Implement Character interface in Player class

--- src/main/java/com/example/comp2522202430termprojectmazzze/Player.java
+++ src/main/java/com/example/comp2522202430termprojectmazzze/Player.java
@@ -1,34 +1,48 @@
 package com.example.comp2522202430termprojectmazzze;
 
-public final class Player implements Movement {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+
+public class Player implements Character, Updatable {
     private Position position;
 
 
-    public Player(final int startX, final int startY) {
-        this.position = new Position(startX, startY);
+    public Player(final Position startPosition) {
+        this.position = startPosition;
+
     }
+    @Override
     public Position getPosition() {
         return position;
     }
 
-    private boolean isValidMove(final Position targetPosition, final boolean[][] maze) {
-        int x = targetPosition.getX();
-        int y = targetPosition.getY();
-        return x >= 0 && x < maze.length
-                && y >= 0 && y < maze[0].length
-                && !maze[x][y];
+    @Override
+    public void move(final Direction direction, final boolean[][] maze) {
+        int newX = position.getX() + direction.getDirectionX();
+        int newY = position.getY() + direction.getDirectionY();
+
+        if (isValidMove(newX, newY, maze)) {
+            position = new Position(newX, newY);
+        }
+    }
+
+
+    private boolean isValidMove(final int targetX, final int targetY, final boolean[][] maze) {
+        return targetX >= 0 && targetX < maze.length
+                && targetY >= 0 && targetY < maze[0].length
+                && !maze[targetX][targetY];
     }
 
     @Override
-    public void move(final int directionX, final int directionY, final boolean[][] maze) {
-        Position nextPosition = new Position(
-                position.getX() + directionX,
-                position.getY() + directionY
-        );
+    public void render(final GraphicsContext gc, int tileSize) {
+        gc.setFill(Color.BLUE);
+        gc.fillRect(position.getX() * tileSize, position.getY() * tileSize, tileSize, tileSize);
+    }
 
-        if (isValidMove(nextPosition, maze)) {
-            position = nextPosition;
-        }
+    @Override
+    public void update(final boolean[][] maze) {
+
     }
 
 }
