4eec7a565b1b4a85fc5e71a3fad81c24cfe36219
eunji-cs <ekwon9@my.bcit.ca>
2024-11-28 17:16:45 -0800

    Refactor ImageLoader to use Singleton pattern for optimizing image caching

--- src/main/java/com/example/comp2522202430termprojectmazzze/ImageLoader.java
+++ src/main/java/com/example/comp2522202430termprojectmazzze/ImageLoader.java
@@ -2,15 +2,44 @@ package com.example.comp2522202430termprojectmazzze;
 
 import javafx.scene.image.Image;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class ImageLoader {
-    public static Image loadImage(final String imagePath) {
+
+    //singleton instance
+    private static ImageLoader instance;
+
+    //image caching
+    private final Map<String, Image> imageCache;
+
+    // private constructor
+    private ImageLoader() {
+        imageCache = new HashMap<>();
+    }
+
+    public static synchronized ImageLoader getInstance() {
+        if (instance == null) {
+            instance = new ImageLoader();
+        }
+        return instance;
+    }
+
+    public Image loadImage(final String imagePath) {
+
+        if (imageCache.containsKey(imagePath)) {
+            return imageCache.get(imagePath);
+        }
+
         try {
-            return new Image(Objects.requireNonNull(
-                    ImageLoader.class.getResourceAsStream(imagePath),
+            Image image = new Image(Objects.requireNonNull(
+                    getClass().getResourceAsStream(imagePath),
                     "Resource not found: " + imagePath
             ));
+            imageCache.put(imagePath, image);
+            return image;
+
         } catch (Exception e) {
             System.out.println("Failed to load image: " + imagePath);
             e.printStackTrace();
